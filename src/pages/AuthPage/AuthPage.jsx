37de0098df9ed90bc5c1f3ac0554556e032754b7
MarinaKarabut <77589480+MarinaKarabut@users.noreply.github.com>
2021-07-27 23:37:42 +0300

    fix styles auth page

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -8,7 +8,8 @@ const  AuthPage = () => {
                 <div className={styles.wrapper}>
                     <div className={styles.authPageTextWrapper}>
                         <h1 className={styles.authPageTitle}>Pro Test</h1>
-                        <p className={styles.authPageText}><span className={styles.authPageSpan}>[</span> We will help you find weak points in knowledge so that you can strengthen it. We will show you what is relevant to know for a <span className={styles.authPageSpan}>QA Engineer</span> and will try to make the learning process more diverse_ <span className={styles.authPageSpan}>]</span></p>   
+                        <p className={styles.authPageText}><span className={styles.authPageSpan}>[</span> We will help you find weak points 
+                        <br/> in knowledge so that you can strengthen it. We will show you what is relevant to know for a <span className={styles.authPageSpan}>QA Engineer</span> and will try to make the learning process more diverse_ <span className={styles.authPageSpan}>]</span></p>   
                     </div>
                     <div><AuthForm/></div>
                 </div>

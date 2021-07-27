a7bf35a384c1d4dd8cec1a790840625bb8e26d51
Mariia Hunko <masha.gunko96@gmail.com>
2021-07-27 18:46:43 +0300

    Created routes

--- src/app/components/App/App.jsx
+++ src/app/components/App/App.jsx
@@ -1,3 +1,10 @@
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Navbar from '../../../client/Navbar'
+
+import { routes } from './routes';
+
 function App() {
     return (<h1>Welcome to Hell!</h1>)
 }

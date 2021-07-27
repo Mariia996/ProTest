bfa744530a358117e7456573953a99eb1baeeba6
Mariia Hunko <masha.gunko96@gmail.com>
2021-07-27 19:47:38 +0300

    Routers done

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,43 +1,13 @@
 /* eslint-disable import/no-anonymous-default-export */
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit"
-
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist"
-import storage from "redux-persist/lib/storage"
+import { configureStore } from "@reduxjs/toolkit"
 
 import authReducer from "./auth/auth-reducer"
 
-
-const middleware = [
-  ...getDefaultMiddleware({
-    serializableCheck: {
-      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-    },
-  }),
-]
-
-const authPersistConfig = {
-  key: "token",
-  storage,
-  whitelist: ["token"],
-}
-
 export const store = configureStore({
   reducer: {
-    auth: persistReducer(authPersistConfig, authReducer),
+    auth:  authReducer,
   },
-  middleware,
   devTools: process.env.NODE_ENV === "development",
 })
 
-export const persistor = persistStore(store)
-
-export default { store, persistor }
+export default { store }

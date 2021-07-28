cf623c0efd5d4b959c4cd4675e286fb71cf411eb
MarinaKarabut <77589480+MarinaKarabut@users.noreply.github.com>
2021-07-28 23:17:09 +0300

    added contacts, contacts card and contacts page

--- src/client/Contacts/components/ContactsList/ContactsList.jsx
+++ src/client/Contacts/components/ContactsList/ContactsList.jsx
@@ -1,5 +1,16 @@
-const ContactsList = () => {
-    return ( <></> );
-}
+import React from 'react';
+
+import { contacts } from '../../../../assets/contacts'
+import ContactsItem from '../ContactsItem'
+
+import styles from './ContactsList.module.scss'
+
+function ContactsList() {
+    const contactsCardEl = contacts.map(({ id, ...contact }) => <ContactsItem key={id} {...contact} />)
+    return (
+        <ul className={styles.contactsList}>
+            {contactsCardEl}
+        </ul>)
+};
 
 export default ContactsList;

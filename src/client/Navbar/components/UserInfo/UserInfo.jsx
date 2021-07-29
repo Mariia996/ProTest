5eb03b48ae9dd27da69a59fdc2c9f1e936444210
MarinaKarabut <77589480+MarinaKarabut@users.noreply.github.com>
2021-07-29 23:04:18 +0300

    added component UserInfo

--- src/client/Navbar/components/UserInfo/UserInfo.jsx
+++ src/client/Navbar/components/UserInfo/UserInfo.jsx
@@ -1,5 +1,18 @@
+import { useSelector, shallowEqual} from 'react-redux';
+import { ReactComponent as LogOut } from '../../../../images/navigation/sign-out.svg';
+
+import styles from './UserInfo.module.scss'
+
 const UserInfo = () => {
-    return ( <></> );
+    // const email = useSelector(state => state.auth.user.email, shallowEqual)
+    const email = "m.k@mail.com"
+
+    return (
+        <div className={styles.containerUser}>
+            <span className={ styles.firstLetter}>{email.slice(0, 1).toUpperCase()}</span>
+            <span className={ styles.email}>{email}</span>
+            <LogOut className={styles.btnLogOut}></LogOut>
+        </div>);
 }
 
 export default UserInfo;
--- src/redux/auth/auth-reducer.js
+++ src/redux/auth/auth-reducer.js
@@ -1,43 +1,52 @@
-import { combineReducers } from "redux"
-import { createReducer } from "@reduxjs/toolkit"
+import { combineReducers } from 'redux';
+import { createReducer } from '@reduxjs/toolkit';
 
-import actions from "./auth-actions"
+import actions from './auth-actions';
 
 const initialStateUser = {
-    email: "",
-  }
-  
-  const initialStateToken = null
-  
-  const initialStateError = null
+  email: '',
+};
 
-  const user = createReducer(initialStateUser, {
-    [actions.registerSuccess]: (_, { payload }) => payload.user,
-    [actions.loginSuccess]: (_, { payload }) => payload.user,
-  })
+const initialStateToken = null;
 
-  const token = createReducer(initialStateToken, {
-    [actions.registerSuccess]: (_, { payload }) => payload.token,
-    [actions.loginSuccess]: (_, { payload }) => payload.token,
-  })
+const initialStateError = null;
 
-  const error = createReducer(initialStateError, {
-    [actions.registerError]: (_, { payload }) => payload,
-    [actions.registerSuccess]: () => initialStateError,
-    [actions.loginError]: (_, { payload }) => payload,
-    [actions.loginSuccess]: () => initialStateError,
-  })
+const user = createReducer(initialStateUser, {
+  [actions.registerSuccess]: (_, { payload }) => payload.user,
+  [actions.loginSuccess]: (_, { payload }) => payload.user,
+  [actions.logoutSuccess]: () => initialStateUser,
+  [actions.getCurrentUserSuccess]: (_, { payload }) => payload.user,
+});
 
-  const isAuthenticated = createReducer(false, {
-    [actions.registerSuccess]: () => true,
-    [actions.loginSuccess]: () => true,
-    [actions.registerError]: () => false,
-    [actions.loginError]: () => false,
-  })
+const token = createReducer(initialStateToken, {
+  [actions.registerSuccess]: (_, { payload }) => payload.token,
+  [actions.loginSuccess]: (_, { payload }) => payload.token,
+  [actions.logoutSuccess]: () => initialStateToken,
+});
 
-  export default combineReducers({
-    user,
-    isAuthenticated,
-    token,
-    error
-  })
+const error = createReducer(initialStateError, {
+  [actions.registerError]: (_, { payload }) => payload,
+  [actions.registerSuccess]: () => initialStateError,
+  [actions.loginError]: (_, { payload }) => payload,
+  [actions.loginSuccess]: () => initialStateError,
+  [actions.logoutError]: (_, { payload }) => payload,
+  [actions.logoutSuccess]: () => initialStateError,
+  [actions.getCurrentUserError]: (_, { payload }) => payload,
+});
+
+const isAuthenticated = createReducer(false, {
+  [actions.registerSuccess]: () => true,
+  [actions.loginSuccess]: () => true,
+  [actions.registerError]: () => false,
+  [actions.loginError]: () => false,
+  [actions.getCurrentUserSuccess]: () => true,
+  [actions.getCurrentUserError]: () => false,
+  [actions.logoutSuccess]: () => false,
+});
+
+export default combineReducers({
+  user,
+  isAuthenticated,
+  token,
+  error,
+});

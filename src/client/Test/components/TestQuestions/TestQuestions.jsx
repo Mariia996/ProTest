0eb7e3be5f5e540ac9dccd81bb7782ef44f943fb
Yaroslav Hliy <hliy.yaroslav@gmail.com>
2021-08-04 10:59:21 +0300

    TestAnswerList  and  TestQuestions

--- src/client/Test/components/TestQuestions/TestQuestions.jsx
+++ src/client/Test/components/TestQuestions/TestQuestions.jsx
@@ -1,17 +1,26 @@
+import { useSelector, shallowEqual } from 'react-redux';
+import { getTests } from '../../../../redux/tests/tests-selectors';
 import CurrentQuestions from '../CurrentQuestions';
 import TestAnswersList from '../TestAnswersList';
 
 import s from './TestQuestions.module.scss';
 
 const TestQuestions = () => {
-    return (
-        <div className={s.test_wrapper}>
-            <div className={s.currentQuestions}>
-                <CurrentQuestions />
-            </div>
-            <p className={s.test_question}>What is regression testing?</p>
-            <TestAnswersList />
-    </div> );
-}
+  const tests = useSelector(state => getTests(state), shallowEqual);
+  return (
+    <div className={s.test_wrapper}>
+      <div className={s.currentQuestions}>
+        <CurrentQuestions />
+      </div>
+      {tests.map(({ _id, question }) => (
+        <p key={_id} className={s.test_question}>
+          {question}
+        </p>
+      ))}
+
+      <TestAnswersList />
+    </div>
+  );
+};
 
 export default TestQuestions;

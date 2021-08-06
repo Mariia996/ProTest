5b950687464ea8a3ad0c60be6fe7fd1c893d3609
MarinaKarabut <77589480+MarinaKarabut@users.noreply.github.com>
2021-08-06 22:37:50 +0300

    added react-toastify

--- src/client/AuthForm/components/AuthForm.jsx
+++ src/client/AuthForm/components/AuthForm.jsx
@@ -7,9 +7,8 @@ import { initialState } from './initialState';
 import { ReactComponent as GoggleSvg } from '../../../images/google.svg';
 import useForm from '../../../shared/hooks/useForm';
 import { logIn, register } from '../../../redux/auth/auth-operations';
-import { error } from '@pnotify/core';
-import '@pnotify/core/dist/PNotify.css';
-import '@pnotify/core/dist/BrightTheme.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import styles from './AuthForm.module.scss';
 
@@ -22,17 +21,16 @@ const AuthForm = () => {
   const onSubmit = data => {
     const action = actionType === 'login' ? logIn(data) : register(data);
     dispatch(action);
-  };
+    };
     
   const [data, , handleChange, handleSubmit] = useForm({ initialState, onSubmit });
     useEffect(() => {
         if (errorCode) {
             const errorMessage = (errorCode.response.status === 409) ? 'You are already registered' : 'Incorrect email or password'
-            console.log(errorMessage);
-            error({
-              text: errorMessage,
-              delay: 2000
-            }); 
+            toast.error(
+            errorMessage,
+            { position: toast.POSITION.TOP_RIGHT },
+          );
         } 
     }, [errorCode])
         
@@ -70,26 +68,27 @@ const AuthForm = () => {
   //     console.log(response.profileObj);
   //   };
 
-  return (
-    <div className={styles.authFormContainer}>
-      <div className={styles.formGroup}>
-        <p className={styles.formGroupText}>
-          You can use your Google Account to authorize:
-        </p>
-        <div className={styles.googleBtnContainer}>
-          <Button onClick={signIn} className={styles.googleBtn}><GoggleSvg className={styles.googleLogo} />Google</Button>
-        </div>
-        <p className={styles.formGroupText}>Or login to our app using e-mail and password:</p>
-        <form onSubmit={handleSubmit}>
-          <Input {...fields.email} value={data.email} onChange={handleChange} />
-          <Input {...fields.password} value={data.password} onChange={handleChange}/>
-          <div className={styles.buttonContainer}>
-            <Button className={styles.button} type="submit" onClick={() => setActionType('login')}>Sign in</Button>
-            <Button className={styles.button} type="submit" onClick={() => setActionType('register')}>Sign up</Button>
-          </div>
-        </form>
-      </div>
-    </div>
+    return (
+        <>
+            <div className={styles.authFormContainer}>
+                <div className={styles.formGroup}>
+                    <p className={styles.formGroupText}>You can use your Google Account to authorize:</p>
+                    <div className={styles.googleBtnContainer}>
+                        <Button onClick={signIn} className={styles.googleBtn}><GoggleSvg className={styles.googleLogo} />Google</Button>
+                    </div>
+                    <p className={styles.formGroupText}>Or login to our app using e-mail and password:</p>
+                    <form onSubmit={handleSubmit}>
+                        <Input {...fields.email} value={data.email} onChange={handleChange} />
+                        <Input {...fields.password} value={data.password} onChange={handleChange}/>
+                        <div className={styles.buttonContainer}>
+                            <Button className={styles.button} type="submit" onClick={() => setActionType('login')}>Sign in</Button>
+                            <Button className={styles.button} type="submit" onClick={() => setActionType('register')}>Sign up</Button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <ToastContainer />
+        </>
   );
 };
 
